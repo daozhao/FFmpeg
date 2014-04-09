d24a162c57a2fe187a9b58ad60e261d87ca84815
daozhao <daozhao@163.com>
2014-04-09 11:51:37 +0800

    添加xcode project方便调试。

---
+++ FFmepgXcode/FFmepgXcode/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  FFmepgXcode
+//
+//  Created by daozhao chen on 4/9/14.
+//  Copyright (c) 2014 daozhao chen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
